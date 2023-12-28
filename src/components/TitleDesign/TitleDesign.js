c3dddd9b1f389bb027f90347f951e2bbca5c33c1
searchsakib <searchsakib@gmail.com>
2023-12-29 03:13:24 +0600

    created a TitleDesign component to design the title

---
+++ src/components/TitleDesign/TitleDesign.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TitleDesign = ({ children }) => {
+  return <h2 className="text-4xl text-white text-center p-7"> {children} </h2>;
+};
+
+export default TitleDesign;
