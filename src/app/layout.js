bb3bd8a1ff01f5b33783cab9890c55b6816fc324
searchsakib <searchsakib@gmail.com>
2023-12-29 11:46:01 +0600

    theme added from daisyui in tailwind config, changed layout.js html data-theme to dim

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" data-theme="dim" suppressHydrationWarning>
       <body className={inter.className} suppressHydrationWarning>
         {children}
       </body>
